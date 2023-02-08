38879b48e35328e5a5395aa34785c1a894af0a7f
yyhjin <wendy0301@naver.com>
2023-02-09 02:05:44 +0900

    230209

--- BaekJoon/Main_B5_1001_AㅡB.java
+++ BaekJoon/Main_B5_1001_AㅡB.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_B5_1001_A빼기B {
+public class Main_B5_1001_AㅡB {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
