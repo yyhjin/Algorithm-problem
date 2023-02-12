7591e337cfdb19ee98f988b87521b66037f6ea90
yyhjin <wendy0301@naver.com>
2023-02-12 20:35:03 +0900

    230212

---
+++ BaekJoon/Main_B4_2440_별찍기3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_2440_별찍기3 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = N; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
