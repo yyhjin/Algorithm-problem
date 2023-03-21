5d7edc3682d012473479643bdae0270b60636b29
yyhjin <wendy0301@naver.com>
2023-03-21 22:17:10 +0900

    230321

---
+++ BaekJoon/Main_B3_2501_약수구하기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B3_2501_약수구하기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int count = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			if(N%i == 0) count++;
+			if(count == K) {
+				System.out.println(i);
+				break;
+			}
+		}
+		
+		if(count != K) System.out.println(0);
+	}
+
+}
