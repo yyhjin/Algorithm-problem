4c9252215a5710e89556528e29c9c660fd7a74bc
yyhjin <wendy0301@naver.com>
2023-04-16 23:10:55 +0900

    230416

---
+++ BaekJoon/Main_B5_2338_긴자리계산.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main_B5_2338_긴자리계산 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		BigInteger A = new BigInteger(br.readLine());
+		BigInteger B = new BigInteger(br.readLine());
+		System.out.println(A.add(B));
+		System.out.println(A.subtract(B));
+		System.out.println(A.multiply(B));
+	}
+
+}
