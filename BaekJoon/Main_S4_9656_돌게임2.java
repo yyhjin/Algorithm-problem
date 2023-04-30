914655ad49ec6c9de2d7e0fc7ccb634e2417eb33
yyhjin <wendy0301@naver.com>
2023-04-30 23:10:26 +0900

    230430

---
+++ BaekJoon/Main_S4_9656_돌게임2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_S4_9656_돌게임2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(N%2 == 0?"SK":"CY");
+	}
+
+}
