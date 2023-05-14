c65096c681d44ab2ad44205317ce0d2177d97731
yyhjin <wendy0301@naver.com>
2023-05-14 22:47:26 +0900

    230514

---
+++ BaekJoon/Main_B5_10171_고양이.java
@@ -0,0 +1,11 @@
+
+public class Main_B5_10171_고양이 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
