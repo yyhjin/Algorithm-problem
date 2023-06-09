e62f5ba09ba841f4ca442e216d0593279efe098a
yyhjin <wendy0301@naver.com>
2023-06-09 20:21:09 +0900

    230609

---
+++ BaekJoon/Main_G5_11758_CCW.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_G5_11758_CCW {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x2 = Integer.parseInt(st.nextToken());
+		int y2 = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int x3 = Integer.parseInt(st.nextToken());
+		int y3 = Integer.parseInt(st.nextToken());
+		
+		double line = (y3-y1)*(x2-x1);
+		double line2 = (y2-y1)*(x3-x1);
+        
+		if(line == line2) System.out.println(0);
+		else if(line > line2) System.out.println(1);
+		else System.out.println(-1);
+	}
+
+}
