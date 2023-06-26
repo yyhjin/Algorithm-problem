de0b80acc31eb819dfc7173a33e34928bce58591
yyhjin <99111030+yyhjin@users.noreply.github.com>
2023-06-26 07:26:07 +0000

    230626

---
+++ BaekJoon/Main_B5_9498_시험성적.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B5_9498_시험성적 {
+
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int N = Integer.parseInt(br.readLine());
+      
+      if(N>=90) System.out.println('A');
+      else if(N>=80) System.out.println('B');
+      else if(N>=70) System.out.println('C');
+      else if(N>=60) System.out.println('D');
+      else System.out.println('F');
+   }
+
+}
