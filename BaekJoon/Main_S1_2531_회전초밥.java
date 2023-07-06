d7f21e551f1d8a12aba119db7b2f1e51e3361faf
yyhjin <wendy0301@naver.com>
2023-07-06 21:35:49 +0900

    230706

--- BaekJoon/Main_S1_2531_회전초밥.java
+++ BaekJoon/Main_S1_2531_회전초밥.java
@@ -1,61 +1,51 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main_S1_2531_회전초밥 {
 
-	static int N, D, K, C;
-	static int[] dish, v;
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-
-		dish = new int[N + K - 1];
-		v = new int[D + 1];
+		
+		int N = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+k-1];
 		
 		for (int i = 0; i < N; i++) {
-			dish[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(br.readLine());;
 		}
+		
+		// 원형으로 만들기
+		for (int i = N; i < N+k-1; i++) {
+			arr[i] = arr[i-N];
+		}
+		
+		HashMap<Integer, Integer> hash = new HashMap<>();
+		for (int i = 0; i < k-1; i++) {
+			hash.put(arr[i], hash.getOrDefault(arr[i], 0)+1);
+		}
+	
+		int max = 0;
+		int left = 0;
+		int right = k-1;
+		while(right<N+k-1) {			
+			hash.put(arr[right], hash.getOrDefault(arr[right], 0)+1);
 
-		// 원형이니까 1차원 배열로 이어붙이기
-		for (int i = N, j = 0; i < N + K - 1; i++, j++) {
-			dish[i] = dish[j];
-		}
-		
-		// 0 ~ K 까지
-		int count = 0;
-		for (int i = 0; i < K; i++) {
-			if (v[dish[i]] == 0)
-				count++;
-			v[dish[i]]++;
-		}
-		
-		int max = count;
-		int start = 1;
-		int end = K;
-		while(start<N && end<N+K-1) {
-			if(v[dish[start-1]]==1) count--;
-			v[dish[start-1]]--;
+			if(hash.containsKey(c)) max = Math.max(max, hash.size());
+			else max = Math.max(max, hash.size()+1);
 			
-			if(v[dish[end]]==0) count++;
-			v[dish[end]]++;
+			if(hash.get(arr[left]) == 1) hash.remove(arr[left]);
+			else hash.put(arr[left], hash.get(arr[left])-1);
 			
-			if (v[C] == 0)  // 쿠폰 초밥 안먹었으면
-				max = Math.max(max, count+1);
-			else   // 쿠폰 초밥 먹었으면
-				max = Math.max(max, count);
-			
-			start++;
-			end++;			
+			left++;
+			right++;
 		}
 		
 		System.out.println(max);
-		
 	}
 		
 }
