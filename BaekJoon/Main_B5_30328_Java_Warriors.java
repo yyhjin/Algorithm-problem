870454ffd4f03b6cbd8919b19fb64d8507349e37
yyhjin <wendy0301@naver.com>
2023-11-17 17:35:43 +0900

    231117

---
+++ BaekJoon/Main_B5_30328_Java_Warriors.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B5_30328_Java_Warriors {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(N*4000);
+	}
+
+}
