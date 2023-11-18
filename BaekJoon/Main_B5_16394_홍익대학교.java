a9b182a142ea90ea6e1170283cf1a3cc1e763995
yyhjin <wendy0301@naver.com>
2023-11-18 23:07:41 +0900

    231118

---
+++ BaekJoon/Main_B5_16394_홍익대학교.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main_B5_16394_홍익대학교 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		int var = sc.nextInt();
+		var -= 1946;
+		System.out.println(var);
+	}
+
+}
