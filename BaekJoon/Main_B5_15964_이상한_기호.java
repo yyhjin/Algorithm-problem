3d2a2749ada671d33de5bb0e4d7d53660b6db99b
yyhjin <wendy0301@naver.com>
2023-11-23 20:00:46 +0900

    231123

---
+++ BaekJoon/Main_B5_15964_이상한_기호.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B5_15964_이상한_기호 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		
+		System.out.println(((A+B)*(A-B)));
+	}
+
+}
